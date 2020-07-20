4f9bf60aff02fc11b8304ea86507bce84c40dbcf
janardhan-boddeti <68552900+janardhan-boddeti@users.noreply.github.com>
2020-07-20 20:48:00 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,11 @@
-print("Hi Janardhan")
+What is python? What are the  ways to install python in your PC?
+
+Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance. Python supports modules and packages, which encourages program modularity and code reuse. Created by Guido van Rossum and first released in 1991.
+Step 1: To install Python, firstly You need to go to the Download Python page from its official site python.org/download and click on the latest version
+Step 2: Once the Python distribution download is completed, then double-click on the executable downloaded software, and then click on Run
+Now, once the installation set up starts.
+To write a Python script file, You can use any text editing software. For that, You just have to save it with the .py extension. But, using a Python IDE can make developers’ life a lot easier. IDE is a software that provides useful features like hinting code, highlighting and checking syntax, file explorers, etc. to programmers for application development.
+
+What are Code Environments ? Types	
+A code environment is a standalone and self-contained environment to run Python 
+As mentioned there are  Pycharm IDE and visual Studio Code Editor we can use to run our python programs.
